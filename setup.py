3aa60e28d17886aded37e1a03394014056786cf4
steve.brazier <steve.brazier@just-eat.com>
2018-06-27 19:53:48 +0100

    shell project

---
+++ setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+setup(
+    name = "talepy",
+    packages = ["talepy"],
+    version = "0.1.0",
+    description = "Distributed Transactions Helper",
+    author = "Steve Brazier",
+    author_email = "meadsteve@gmail.com",
+    url = "https://github.com/meadsteve/talepy",
+    keywords = ["saga", "distributed transaction"],
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        ],
+    long_description = """\
+Tale Distributed Transactions
+-------------------------------------
+
+"""
+)
---
+++ tests/test_pass.py
@@ -0,0 +1,2 @@
+def test_helloness():
+    assert "world" == "world"

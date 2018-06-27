e7acdae0c111f275514894d65194c7a3e8c03e83
steve.brazier <steve.brazier@just-eat.com>
2018-06-27 20:11:48 +0100

    simplification

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -ex
 
-py.test tests
-mypy --ignore-missing-imports --strict-optional --check-untyped-defs talepy/
-flake8 talepy tests
-isort --recursive --check-only talepy tests
+pipenv run py.test tests
+pipenv run mypy --ignore-missing-imports --strict-optional --check-untyped-defs talepy/
+pipenv run flake8 talepy tests
+pipenv run isort --recursive --check-only talepy tests

6c4404f9154543ccc79c83e46484183ec1563ce3
steve.brazier <steve.brazier@just-eat.com>
2018-07-02 20:18:48 +0100

    Fix test for import paths

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -ex
 
-pipenv run py.test tests
+pipenv run python -m pytest tests/
 pipenv run mypy --ignore-missing-imports --strict-optional --check-untyped-defs talepy/
 pipenv run flake8 talepy tests
 pipenv run isort --recursive --check-only talepy tests

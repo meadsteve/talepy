181aef5bfc225917775c1ec17a4a9000b865915a
steve.brazier <steve.brazier@just-eat.com>
2018-07-02 21:14:07 +0100

    type check tests

--- test.sh
+++ test.sh
@@ -2,6 +2,6 @@
 set -ex
 
 pipenv run python -m pytest tests/
-pipenv run mypy --ignore-missing-imports --strict-optional --check-untyped-defs talepy/
+pipenv run mypy --ignore-missing-imports --strict-optional --check-untyped-defs talepy/ tests/
 pipenv run flake8 talepy tests
 pipenv run isort --recursive --check-only talepy tests

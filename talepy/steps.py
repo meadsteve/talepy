917eaddb9c468b0a41972acb5f6dead8e22eb8ef
steve.brazier <steve.brazier@just-eat.com>
2018-07-04 21:04:09 +0100

    Add wrapper for lambdas to be steps

--- talepy/steps.py
+++ talepy/steps.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import Generic, TypeVar
+from typing import Any, Callable, Generic, TypeVar
 
 InputState = TypeVar('InputState')
 OutputState = TypeVar('OutputState')
@@ -14,3 +14,23 @@ class Step(ABC, Generic[InputState, OutputState]):
     @abstractmethod
     def compensate(self, state: OutputState) -> None:
         pass
+
+
+Y = TypeVar('Y')
+
+
+class LambdaStep(Step[Any, Y]):
+
+    def __init__(
+        self,
+        execute_lambda: Callable[[Any], Y],
+        compensate_lambda: Callable[[Y], Any] = None
+    ) -> None:
+        self.execute_lambda = execute_lambda
+        self.compensate_lambda = compensate_lambda or (lambda _x: None)
+
+    def execute(self, state):
+        return self.execute_lambda(state)
+
+    def compensate(self, state) -> None:
+        self.compensate_lambda(state)
---
+++ tests/test_lambda_steps.py
@@ -0,0 +1,23 @@
+from talepy.steps import LambdaStep
+
+
+def test_execute_calls_the_first_lambda():
+    step = LambdaStep(lambda x: f"hello {x}", lambda y: None)
+    assert step.execute("world") == "hello world"
+
+
+def test_compensate_calls_the_second_lambda_with_the_supplied_state():
+    class Reverter:
+        def lambda_func(self, state_given):
+            self.reverted = state_given
+    reverter = Reverter()
+    step = LambdaStep(lambda x: f"hello {x}", reverter.lambda_func)
+
+    step.compensate("world")
+    assert reverter.reverted == "world"
+
+
+def test_compensate_step_is_optional():
+    step = LambdaStep(lambda x: f"hello {x}")
+    assert step.execute("world") == "hello world"
+    step.compensate("world")

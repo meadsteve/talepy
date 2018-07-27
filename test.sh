65208236e7ac82a9032f7021baac9b3d25c4a1a6
steve.brazier <steve.brazier@just-eat.com>
2018-07-27 16:00:14 +0100

    remove isort

--- test.sh
+++ test.sh
@@ -4,4 +4,3 @@ set -ex
 pipenv run python -m pytest tests/
 pipenv run mypy --ignore-missing-imports --strict-optional --check-untyped-defs talepy/ tests/
 pipenv run black --check talepy tests
-pipenv run isort --recursive --check-only talepy tests

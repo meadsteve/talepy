f3d989359362dbad33c9496ac6bc7a1a7b198e20
steve <meadsteve@gmail.com>
2020-02-03 09:34:00 +0100

    add missing format command

--- tests/test_async_transactions.py
+++ tests/test_async_transactions.py
@@ -6,7 +6,7 @@ from talepy.exceptions import (
     AsyncStepUsedInSyncTransaction,
     RetriesCannotBeUsedInAsync,
 )
-from talepy.parallel import run_async_transaction
+from talepy.parallel import run_concurrent_transaction
 from talepy.retries import attempt_retries
 from tests.mocks import (
     MockCountingStep,
@@ -21,7 +21,7 @@ from tests.mocks import (
 @pytest.mark.asyncio
 async def test_a_transaction_runs_a_single_step_it_wraps():
     mock_step = MockCountingStep()
-    await run_async_transaction(steps=[mock_step], starting_state=0)
+    await run_concurrent_transaction(steps=[mock_step], starting_state=0)
 
     assert mock_step.actions_taken == ["run execute: 0"]
 
@@ -31,7 +31,7 @@ async def test_a_transaction_runs_many_steps_it_wraps():
     step_1 = MockCountingStep()
     step_2 = MockCountingStep()
     step_3 = MockCountingStep()
-    await run_async_transaction(steps=[step_1, step_2, step_3], starting_state=0)
+    await run_concurrent_transaction(steps=[step_1, step_2, step_3], starting_state=0)
 
     assert step_1.actions_taken == ["run execute: 0"]
     assert step_2.actions_taken == ["run execute: 0"]
@@ -45,7 +45,9 @@ async def test_if_any_step_fails_they_all_get_rolled_back():
     step_3 = MockCountingStep()
 
     try:
-        await run_async_transaction(steps=[step_1, step_2, step_3], starting_state=0)
+        await run_concurrent_transaction(
+            steps=[step_1, step_2, step_3], starting_state=0
+        )
     except AsyncStepFailures as _e:
         pass
 
@@ -60,7 +62,7 @@ async def test_all_exceptions_are_returned_to_the_user():
     step_2 = AlwaysFailsStep()
 
     with pytest.raises(AsyncStepFailures) as caught_error:
-        await run_async_transaction(steps=[step_1, step_2], starting_state=0)
+        await run_concurrent_transaction(steps=[step_1, step_2], starting_state=0)
 
     assert caught_error.value.inner_exceptions == [step_1.exception, step_2.exception]
 
@@ -69,7 +71,7 @@ async def test_all_exceptions_are_returned_to_the_user():
 async def test_steps_may_be_async():
     step_1 = MockCountingStep()
     step_2 = MockAsyncExecuteStep()
-    await run_async_transaction(steps=[step_1, step_2], starting_state=0)
+    await run_concurrent_transaction(steps=[step_1, step_2], starting_state=0)
 
     assert step_1.actions_taken == ["run execute: 0"]
     assert step_2.actions_taken == ["run execute: 0"]
@@ -82,7 +84,9 @@ async def test_compensate_funcs_can_be_async():
     step_3 = MockAsyncCompensateStep()
 
     try:
-        await run_async_transaction(steps=[step_1, step_2, step_3], starting_state=0)
+        await run_concurrent_transaction(
+            steps=[step_1, step_2, step_3], starting_state=0
+        )
     except AsyncStepFailures as _e:
         pass
 
@@ -105,7 +109,7 @@ async def test_currently_retries_cant_be_used_async():
     step_1 = MockCountingStep()
 
     with pytest.raises(RetriesCannotBeUsedInAsync):
-        await run_async_transaction(
+        await run_concurrent_transaction(
             steps=[attempt_retries(step_1, times=2)], starting_state=0
         )
 
@@ -116,6 +120,6 @@ async def test_steps_are_treated_as_coroutines():
     # Each slowmo step does an asyncio.sleep for 1 second so 2000
     # of these can complete quickly if our async runner is working
     steps = [SlowMoStep() for _ in range(0, 2000)]
-    results = await run_async_transaction(steps=steps, starting_state=0)
+    results = await run_concurrent_transaction(steps=steps, starting_state=0)
 
     assert all([r == "okay" for r in results])
